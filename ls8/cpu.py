4549a265390382debe4f78e9c9865e423318f6d5
Eli Citron <ebcitron@gmail.com>
2020-02-26 13:47:41 -0600

    Working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -19,8 +19,6 @@ class CPU:
         self.PC = 0 #Program Counter, address of the currently executing instruction
         self.MAR = 0 #Memory Address Register, holds the memory address we're reading or writing
         self.MDR = 0 #Memory Data Register, holds the value to write or the value just read
-
-
         self.SP = 7
       
         self.cmds = {
@@ -31,6 +29,11 @@ class CPU:
         
         
     def ram_read(self, MAR):
+        # print("\n------------------------")
+        # print(f"ram_read()")
+        # print(f"self.ram[{MAR}]: ", self.ram[MAR])
+        # print("------------------------\n")
+        
         return self.ram[MAR]
 
     def ram_write(self, MAR, MDR):
@@ -40,12 +43,17 @@ class CPU:
         return(0, False)
 
     def ldi(self, opp_a, opp_b):
+        print(f"ldi(self, opp_a, opp_b): ldi({self}, {opp_a}, {opp_b}")
+        print("Virgin self.reg: ", self.reg)
         self.reg[opp_a] = opp_b
+        print("Self.reg: ", self.reg)
         return (3, True)
 
     def prn(self, opp_a, opp_b):
+        print(f"prn({opp_a}, {opp_b}")
         print(self.reg[opp_a])
         return (2, True)
+
 
 
     def load(self, program):
@@ -55,32 +63,45 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # programx = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        with open() as f:
+        with open(program) as f:
+            print("\n------------------------")
+            print("load() \n")
             for line in f:
-                if line[0] != '#' and line != '\n':
-                    self.ram[address] = int(line[0:8], 2)
-                    address += 1
-                f.closed
-            print(self.ram)
+                #Clean comments out and strip the commands of trailing spaces
+                prepped = line.split("#") 
+                number = prepped[0].strip()
 
+                print(f"Number: {number}")
+                try:
+                    self.ram[address] = int(number[0:8], 2)
+                    address += 1
+                except ValueError:
+                    print(f"Value Error: {ValueError}")
+                    pass
+            print("\n------------------------")
+        #         if line[0] != '#' and line != '\n':
+        #             self.ram[address] = int(line[0:8], 2)
+        #             address += 1
+        #         f.closed
+        #     print(self.ram)
             
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        # for instruction in programx:
+        #     self.ram[address] = instruction
+        #     address += 1
 
     def alu(self, op, reg_a, reg_b):
+
         """ALU operations."""
 
         if op == "ADD":
@@ -105,7 +126,9 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
+        
+        print("trace() \n")
+        print("self.PC | self.PC, self.PC + 1, self.PC + 2 |")
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.PC,
             #self.fl,
@@ -115,26 +138,39 @@ class CPU:
             self.ram_read(self.PC + 2)
         ), end='')
 
+        print("\n------------------------")
+        
+        print("self.reg[i] in range(8)")
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
+
+        print("\n------------------------")
 
         print()
 
     def run(self):
         """Run the CPU."""
         running = True
-
+        print("\n------------------------")
+        print("run() \n")
+        self.trace()
+        print(f"Program Counter: {self.PC}")
+        self.PC = 0
         while running:
             IR = self.ram_read(self.PC) #Set Instruction Register
             print("IR: ", IR)
+            print(f"self.pc: {self.PC}")
+            print(f"Self.ram: {self.ram}")
 
             opp_a = self.ram_read(self.PC + 1)
             opp_b = self.ram_read(self.PC + 2)
             
             try:
-                operation = self.cmds[IR](opp_a, opp_b)
-                running = operation[1]
-                self.PC += operation
+                op= self.cmds[IR](opp_a, opp_b)
+                running = op[1]
+                self.PC += op
+                print("TRY self.pc: ", self.PC)
+                print("TRY operation: ", op)
 
             except:
                 print(f"Error: IR {IR} not available")
